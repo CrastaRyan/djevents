f2119f427d283f5df9659d27160e937b0d341428
Ryan C <ryan4121999@gmail.com>
2022-04-05 14:14:11 +0530

    pages & routing

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <p>
+            This is an app to fnd the latest DJ and other musical events
+        </p>
+        <p>
+            Version 1.0.0
+        </p>
+
+    </div>
+  )
+}
+export default About;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventsPage = () => {
+  return (
+    <div>My events</div>
+  )
+}
+
+export default EventsPage
